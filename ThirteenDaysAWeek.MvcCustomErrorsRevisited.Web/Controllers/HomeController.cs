158d1cd1b809290a17bbd9325c2d697d4cc3c617
Christopher House <chris.house.00@gmail.com>
2013-08-27 09:12:36 -0500

    Updated stuff

---
+++ ThirteenDaysAWeek.MvcCustomErrorsRevisited.Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThirteenDaysAWeek.MvcCustomErrorsRevisited.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            //throw new NotImplementedException();
+            return View();
+        }
+
+        public ActionResult Http500()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Http403()
+        {
+            return new HttpUnauthorizedResult();
+        }
+
+        public ActionResult Http404()
+        {
+            return new HttpNotFoundResult();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
